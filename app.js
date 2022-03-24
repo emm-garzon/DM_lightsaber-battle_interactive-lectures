277f4758fd35cc2919686cc73d90184203673b53
Emmanuel Garzon <eggarzon@gmail.com>
2022-03-23 22:12:36 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+let obiWanAttack = 35;
+let anakinAttack = 25;
+
+let anakinHealth = 100;
+let anakinDefense = 0;
+
+if (anakinAttack > obiWanAttack) {
+  console.log("Anakin has the stronger attack!");
+} else if (obiWanAttack > anakinAttack) {
+  console.log("Obi-wan has the higher attack!");
+} else {
+  console.log("The force is with both of them.");
+}
+
+if (anakinHealth <= 0) {
+  console.log("Anakin has fallen. #nolegs");
+} else {
+  anakinHealth -= obiWanAttack;
+  console.log(`Anakin's health is now: ${anakinHealth}`);
+}
+
+console.log(anakinDefense); //0
+anakinDefense += 25;
+console.log(anakinDefense); // 25
+
+if (anakinHealth <= obiWanAttack - anakinDefense) {
+  console.log("Anakin has fallen. #oneMetalArm");
+} else {
+  anakinHealth -= obiWanAttack - anakinDefense;
+  console.log(`Anakin's health is now: ${anakinHealth}`);
+}
+
+console.log("Anakin takes a medpac");
+
+if (anakinHealth + 50 >= 100) {
+  anakinHealth = 100;
+  console.log(anakinHealth);
+} else {
+  anakinHealth += 50;
+  console.log(anakinHealth);
+}
+
+for (let i = 0; i < 5; i++) {
+  anakinHealth -= obiWanAttack - anakinDefense;
+  console.log(`Anakin's health is now: ${anakinHealth}`);
+}
+
+while (anakinHealth > 0) {
+  anakinHealth -= obiWanAttack - anakinDefense;
+  console.log(`Anakin's health is now: ${anakinHealth}`);
+  if (anakinHealth <= 0) {
+    console.log(`Anakin has fallen to the dark side`);
+  }
+}
